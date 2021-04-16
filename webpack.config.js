867dd2c3bb399b4b4219c7b912cb0ec7c64378d1
Ben Cyna <benjamin.cyna@student.adelaide.edu.au>
2021-04-16 21:14:39 +0930

    added webpack config

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+
+const config = {
+  mode: "production",
+  entry: "/public/index.js",
+  output: {
+    path: __dirname + "/public/dist",
+    filename: "bundle.js",
+  },
+  plugins: [
+    new WebpackPwaManifest({
+      fingerprints: false,
+      inject: false,
+      name: "Budget App",
+      short_name: "BA",
+      description: "An application to track budget",
+      background_color: "#01579b",
+      theme_color: "#ffffff",
+      start_url: "/",
+      icons: [
+        {
+          src: path.resolve("public/icons/icon-192x192.png"),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join("assets", "icons"),
+        },
+      ],
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+};
+module.exports = config;
